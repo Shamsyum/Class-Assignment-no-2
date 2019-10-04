6e1fd11e45838a1d3b452ce5f7da56d21e6dec80
Shams Ur Rahman <shams.programmer@gmail.com>
2019-10-04 15:28:24 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,216 @@
+
+
+// Dear Mentor, I have commented all the questions of both the files of assignments here.
+// If you want to run a specefic question then just uncomment that one question.
+// Uncomminting the whole file or more then one question will not work on the browser!
+// thank you.  
+
+
+
+/*
+
+//Q no. 1  Assignment chapters #9-11
+var cityName = prompt("City Name?");
+if(cityName === "karachi"){
+    alert("Welcome to karachi");
+}
+
+
+//Q no. 2  Assignment chapters #9-11
+var gender = prompt("Your gender ?");
+if(gender == "male"){
+    alert("Good morning sir!");
+}else{
+    alert("Good morning Ma'am!")
+}
+
+
+//Q no. 3  Assignment chapters #9-11
+var signalColor = prompt("Signal Color ?");
+if(signalColor == "red"){
+    alert("Must Stop!");
+}else if(signalColor == "yellow"){
+    alert("Ready to move")
+}else if(signalColor == "green"){
+    alert("Move now !");
+}
+
+
+//Q no. 4  Assignment chapters #9-11
+var remainingFuel = prompt("Rmaining Liters of fuel in the car ?");
+if(remainingFuel < 0.25){
+    alert("Please refill the fuel in your car");
+}
+
+
+//Q no. 5  Assignment chapters #9-11
+  
+    a.  given condition for variable a is true
+    b.  NIL
+    c.  condition 2 is true
+        condition 4 is true
+    d.  The cost equals
+    e.  True
+    f.  car is smaller than cat
+ 
+
+
+//Q no. 6  Assignment chapters #9-11
+var obtainedMarks = prompt ("Marks obtained in three subjects?");
+var totalMarks = prompt ("Total Marks?");
+var percentage = (obtainedMarks * 100) / totalMarks;
+var grade = "A";
+var remarks = "Excelent";
+
+if(percentage >= 80){
+    grade = "A-One";
+    remarks = "Excelent";
+}else if(percentage >= 70){
+    grade = "A";
+    remarks "Good";
+}else if(percentage >= 60){
+    grade = "B";
+    remarks = "You need to improve";
+}else if(percentage < 60){
+    grade = "Fail";
+    remarks = "Sorry";
+}
+
+document.write("Total Marks: " + totalMarks + "<br>");
+document.write("Marks Obtained: " + obtainedMarks + "<br>");
+document.write("Percentage: " + percentage + "%" + "<br>");
+document.write("Grade: " + grade + "<br>");
+document.write("Remarks: " + remarks);
+
+
+//Q no. 7  Assignment chapters #9-11
+var guess = 4;
+var userInput = prompt ("What is the secret number? Ranging (1-10)");
+
+if(userInput === guess){
+    alert("Bingo! Correct Answer");
+}else if(userInput === --guess){
+    alert("Close enough to the correct answer");
+}
+
+
+//Q no. 8  Assignment chapters #9-11
+var num = prompt ("Enter a Number to find divisibility with 3");
+var divs = num % 3;
+if(divs == 0){
+    alert("Its divisible with 3");
+}else{
+    alert("it is not divisible with 3");
+}
+
+
+//Q no. 9  Assignment chapters #9-11
+var num = prompt("Enter a number to check wether it is ever or odd");
+if(num % 2 == 1){
+    alert("It is a odd number");
+}else{
+    alert("It is the even number");
+}
+
+
+//Q no. 10  Assignment chapters #9-11
+var temperature = prompt ("What is the temprature ?");
+if(temperature > 40){
+    alert("It is very hot outside!");
+}else if(temperature > 30){
+    alert("The wether today is normal");
+}else if(temperature > 20){
+    alert("Todays Weather is cool");
+}else if(temperature >10){
+    alert("OMG! Today's weather is so Cool.");
+}
+
+
+//Q no. 11  Assignment chapters #9-11
+var firstNumber = prompt("Enter the first Number:");
+var secondNumber = prompt("Enter the second number:");
+var operator = prompt("Enter opertator, Which operation you want to perfort eg. + - 8 /");
+
+if(operator == "+"){
+    alert(firstNumber + secondNumber);
+}else if(operator == "-"){
+    alert(firstNumber - secondNumber);
+}else if(operator == "*"){
+    alert(firstNumber * secondNumber);
+}else if(operator == "/"){
+    alert(firstNumber / secondNumber);
+}
+
+//___________________________________________________________________________________________________________________________
+
+
+// Q no. 2 Assignment chapters #12-13
+var num1 = prompt ("First Number?");
+var num2 = prompt ("Second Number?");
+if(num1 === num2){
+    alert("Both are equal");
+}else if(num1 > num2){
+    alert (num1);
+}else if(num2 > num1){
+    alert(num2);
+}
+
+
+// Q no. 3 Assignment chapters #12-13
+var num = prompt("Enter a Number");
+if(num > 0){
+    alert("It is a positive number");
+}else if(num < 0){
+    alert("The number is negetive");
+}else{
+    alert("The Number is Zero");
+}
+
+
+// Q no. 4 Assignment chapters #12-13
+var char = prompt("Enter a single character");
+if(char === "a" || char === "e" ||char === "o" ||char === "u"){
+    alert("The character is Vowel");
+}else{
+    alert("The charactor is Consonent");
+}
+
+
+// Q no. 5 Assignment chapters #12-13
+var a;
+var validPass = 1234;
+var enterPass = prompt("Enter the four digit passward");
+if(enterPass == validPass){
+    alert("Correct! The password you entered matches the original password");
+}else if(enterPass !== validPass){
+    alert("Incorrect passward");
+}else if(enterPass == null){
+    alert("Please enter your pasward");
+}
+
+
+// Q no. 6 Assignment chapters #12-13
+var greeting;
+var hour = 13;
+if (hour < 18){
+greeting = "Good day";
+}else{
+    greeting = "Good evening";
+}
+alert (greeting);
+
+
+// Q no. 7 Assignment chapters #12-13
+var time = prompt ("What is time? (Use 24hrs formate like 1900)");
+if(time >= 0000 && time <= 1200){
+    alert("Good Morning");
+}else if(time >= 1200 && time <= 1700){
+    alert("Good Afternoon");
+}else if(time >= 1700 && time <= 2100){
+    alert("Good Evening");
+}else if(time >= 2100 && time <= 2359){
+    alert("Good Night");
+}
+
+
+*/
